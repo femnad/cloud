89510990c737e6a51b180d2ec70b6919f91cadaa
fcd <1352288+femnad@users.noreply.github.com>
2023-12-28 20:38:48 +0300

    fix monitor URL

--- mta-sts/uptime.tf
+++ mta-sts/uptime.tf
@@ -2,5 +2,5 @@ resource "uptimerobot_monitor" "mta-sts" {
   friendly_name = "mta-sts"
   type          = "http"
   sub_type      = "https"
-  url           = "http://${local.dns_name}/.well-known/mta-sts.txt"
+  url           = "http://${trimsuffix(local.dns_name, ".")}/.well-known/mta-sts.txt"
 }
