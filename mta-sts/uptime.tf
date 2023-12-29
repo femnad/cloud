4b9653204b671bc7bbd3470dd5e4ecde9c39895d
fcd <1352288+femnad@users.noreply.github.com>
2023-12-29 16:16:05 +0300

    set interval to 1 hour

--- mta-sts/uptime.tf
+++ mta-sts/uptime.tf
@@ -1,5 +1,6 @@
 resource "uptimerobot_monitor" "mta-sts" {
   friendly_name = "mta-sts"
+  interval      = 3600
   type          = "http"
   url           = "https://${trimsuffix(local.dns_name, ".")}/.well-known/mta-sts.txt"
 }
